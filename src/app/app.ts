37502799a8987b63b68ef3034ec4d8f2f1ccfd50
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-02-20 02:57:05 +0530

    navbar,hero,footer ui

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from "./core/navbar/navbar";
+import { Hero } from "./core/hero/hero";
+import { Footer } from "./core/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Hero, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
